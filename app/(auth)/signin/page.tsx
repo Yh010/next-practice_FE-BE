f2d3a633294187b5bdbc9e8a93cc25d3003eb8ec
Yash Hegde <yash4success@gmail.com>
2024-07-11 16:20:48 +0530

    checking loader

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@ import SigninComponent from "@/components/SigninComponent";
 export default async function Signin() {
   return (
     <div>
+      {await new Promise((r) => setTimeout(r, 5000))}
       <SigninComponent />
     </div>
   );
